57092155e9c545b128f33688338026ad4716b28b
daminidave <60508351+daminidave@users.noreply.github.com>
2023-07-08 18:16:40 +0530

    Update Damini_Dave_CV.py

--- Damini_Dave_CV.py
+++ Damini_Dave_CV.py
@@ -7,8 +7,8 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "DaminiDave_Python Developer.pdf"
-profile_pic = current_dir / "assets" / "profile-pic.png"
+resume_file = current_dir / "DaminiDave_Python Developer.pdf"
+profile_pic = current_dir / "profile-pic.png"
 
 
 # --- GENERAL SETTINGS ---
